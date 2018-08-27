1046106dea62a7a193acf795796a4c6322a2d24c
Notw <siqing95@outlook.com>
2018-08-27 18:07:58 +0800

    tushare更新了专业版数据，观察发现，专业版中可以方便的获得我们想要的数据．同时，获取不再受到访问限制．因此将数据库转化为与专业版接口 １．更新了新的数据接口：股票列表，交易日历，股票单股历史数据爬取 ２．下一步要继续更新数据库部分，因为数据量变大，要尝试减小io时间，尽量减少操作次数．

--- DB/launch.py
+++ DB/launch.py
@@ -22,13 +22,22 @@ sys.setdefaultencoding('utf-8')
 '''
 脚本用于初始化项目数据库以及更新项目数据库
 '''
+def date_pre(date):
+    date = '%s-%s-%s' % (date[0:4], date[4:6], date[6:8])
+    date = datetime.datetime.strptime(date, '%Y-%m-%d')
+    return date
 class launcher(object):
 
-    def __init__(self,DB_name):
+    def __init__(self):
         '''
         :param DB_name:主要是为了方便拓展数据模块，允许用户使用多个不同的数据集
         '''
-        db_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], DB_name)
+        #导入项目配置
+        import settings
+        c = settings.ConfigModule()
+        self._settings = c.settings
+        #链接数据库
+        db_path = self._settings['DB']['db_path']
         engine = create_engine('sqlite:///%s' % db_path, echo=False, encoding='utf-8')
         Sesstion = sessionmaker(bind=engine)
         self._session = Sesstion()
@@ -43,80 +52,153 @@ class launcher(object):
     def log(self, string):
         print('updater:' + string)
 
+    def update_trade_calendar(self):
+        '''
+        先清空数据库中的交易日历再重新插入
+        :return:
+        '''
+        #删除数据
+        temp = self._session.query(Trade_calendar).all()
+        for item in temp:
+            self._session.delete(item)
+        self._session.commit()
+        #插入数据
+        ts.set_token(self._settings['DB']['ts_token'])
+        pro = ts.pro_api()
+        df = pro.trade_cal()
+        for i in range(len(df)):
+            exchange_id = df.loc[i]['exchange_id']
+            cal_date = df.loc[i]['cal_date']
+            id = exchange_id + cal_date
+            #处理日期
+            cal_date = '%s-%s-%s' % (cal_date[0:4], cal_date[4:6], cal_date[6:8])
+            cal_date = datetime.datetime.strptime(cal_date, '%Y-%m-%d')
+            is_open = df.loc[i]['is_open']
+            cal_item = Trade_calendar(id=id,
+                                      exchange_id=exchange_id,
+                                      cal_date=cal_date,
+                                      isopen=is_open)
+            self._session.add(cal_item)
+        self._session.commit()
+
+
     def update_stocks(self):
         '''
         基于tushare数据，更新股票列表数据
+        先删除数据库中的股票列表，再重新插入
         :return:
         '''
-        df = ts.get_stock_basics()
-        for security in df.index:
-            print(security)
-            '''
-            如果数据库中有该股票的基本信息，正常，否则会引发异常
-            '''
-            try:
-                self._session.query(Stock).filter(Stock.security == security).one()
-            except:
-                name = df.ix[security]['name']
-                name = unicode(name)
-                stock = Stock(security=security, name=name)
-                self._session.add(stock)
-                self._session.commit()
+        #删除数据
+        temp = self._session.query(Stock).all()
+        for item in temp:
+            self._session.delete(item)
+        self._session.commit()
+        #插入数据
+        ts.set_token(self._settings['DB']['ts_token'])
+        pro = ts.pro_api()
+        df = pro.stock_basic( fields='ts_code,symbol,name,exchange_id,list_status,list_date,delist_date,list_status,is_hs')
+        for i in range(len(df)):
+            ts_code = df.loc[i]['ts_code']
+            security = df.loc[i]['symbol']
+            name = unicode(df.loc[i]['name'])
+            exchange_id = df.loc[i]['exchange_id']
+            list_status = df.loc[i]['list_status']
+            #处理上市时间
+            list_date = df.loc[i]['list_date']
+            list_date = '%s-%s-%s' % (list_date[0:4],list_date[4:6],list_date[6:8])
+            list_date = datetime.datetime.strptime(list_date,'%Y-%m-%d')
+            #处理休市时间
+            delist_date = df.loc[i]['delist_date']
+            if delist_date:
+                delist_date = '%s-%s-%s' % (delist_date[0:4],delist_date[4:6],delist_date[6:8])
+                datetime.datetime.strptime(delist_date, '%Y-%m-%d')
+            is_hs = df.loc[i]['is_hs']
+            #存入数据库
+            stock_item = Stock(code=ts_code,
+                               security=security,
+                               name=name,
+                               exchange_id=exchange_id,
+                               list_status=list_status,
+                               list_date=list_date,
+                               delist_date = delist_date,
+                               is_hs = is_hs)
+            self._session.add(stock_item)
+        self._session.commit()
 
-    def update_stock_history(self, security,dic = None):
+    def update_stock_history(self,ts_code):
         '''
         基于tushare数据，更新股票历史信息
         :param security:
         :return:
         '''
-        stock = self._session.query(Stock).filter(Stock.security == security).one()  # 获取股票基本信息
-        date_list = [history.date for history in
-                     self._session.query(StockHistory).filter(StockHistory.stock == stock.security).all()]
-        try:
-            df = ts.get_hist_data(security)
-        except:
-            self.log('请求超时，正在重试。。。')
-            time.sleep(300)
-            self.update_stock_history(security)
-            return
-        if isinstance(df, pd.DataFrame) and df.empty:#tushare股票数据为空
-            return False
-        elif isinstance(df, pd.DataFrame) and not df.empty:#tushare股票数据不为空
-            for date in df.index:
-                if pd.to_datetime(date) not in date_list:
-                    if not dic:
-                        pass
-                    else:
-                        for key in dic:
-                            df[key] = dic[key]
-                    if 'turnover' in df.columns:
-                        turnover = df.ix[date]['turnover']
-                    else:
-                        turnover = -1
-                    stock_history_item = StockHistory(id='%s/%s' % (security, date),
-                                                      date=datetime.datetime.strptime(date, '%Y-%m-%d'),
-                                                      open=df.ix[date]['open'],
-                                                      close=df.ix[date]['close'],
-                                                      high=df.ix[date]['high'],
-                                                      low=df.ix[date]['low'],
-                                                      volume=df.ix[date]['volume'],
-                                                      price_change=df.ix[date]['price_change'],
-                                                      p_change=df.ix[date]['p_change'],
-                                                      ma5=df.ix[date]['ma5'],
-                                                      ma10=df.ix[date]['ma10'],
-                                                      ma20=df.ix[date]['ma20'],
-                                                      v_ma5=df.ix[date]['v_ma5'],
-                                                      v_ma10=df.ix[date]['v_ma10'],
-                                                      v_ma20=df.ix[date]['v_ma20'],
-                                                      turnover=turnover)
-                    stock.history.append(stock_history_item)
-                else:
-                    pass
-            self._session.commit()
-            return True
-        else:
-            return False
+        ts.set_token(self._settings['DB']['ts_token'])
+        pro = ts.pro_api()
+        history_data = pro.daily(ts_code = ts_code).set_index('trade_date')
+        history_fractor = pro.daily_basic(ts_code = ts_code).set_index('trade_date')
+        #print history_data
+        for date in history_data.index:
+            id = ts_code+date
+            trade_date = date_pre(date)
+            open = history_data.loc[date]['open']
+            close = history_data.loc[date]['close']
+            high = history_data.loc[date]['high']
+            low = history_data.loc[date]['low']
+            price_change = history_data.loc[date]['change']
+            pct_change = history_data.loc[date]['pct_change']
+            volume = history_data.loc[date]['vol']
+            amount = history_data.loc[date]['amount']
+            if date in history_fractor.index:
+                turnover_rate = history_fractor.loc[date]['turnover_rate']
+                volume_ratio = history_fractor.loc[date]['volume_ratio']
+                pe = history_fractor.loc[date]['pe']
+                pe_ttm = history_fractor.loc[date]['pe_ttm']
+                pb = history_fractor.loc[date]['pb']
+                ps = history_fractor.loc[date]['ps']
+                ps_ttm = history_fractor.loc[date]['ps_ttm']
+                total_share = history_fractor.loc[date]['total_share']
+                float_share = history_fractor.loc[date]['float_share']
+                free_share = history_fractor.loc[date]['free_share']
+                total_mv = history_fractor.loc[date]['total_mv']
+                circ_mv = history_fractor.loc[date]['circ_mv']
+            else:
+                turnover_rate = None
+                volume_ratio = None
+                pe = None
+                pe_ttm = None
+                pb = None
+                ps = None
+                ps_ttm =None
+                total_share = None
+                float_share = None
+                free_share =None
+                total_mv =None
+                circ_mv = None
 
+            history_item = StockHistory(id=id,
+                                        date = trade_date,
+                                        open = open,
+                                        close = close,
+                                        high = high,
+                                        low = low,
+                                        price_change=price_change,
+                                        pct_change=pct_change,
+                                        volume=volume,
+                                        amount=amount,
+                                        turnover_rate=turnover_rate,
+                                        volume_ratio=volume_ratio,
+                                        pe=pe,
+                                        pe_ttm = pe_ttm,
+                                        pb=pb,
+                                        ps=ps,
+                                        ps_ttm = ps_ttm,
+                                        total_share=total_share,
+                                        float_share=float_share,
+                                        free_share=free_share,
+                                        total_mv=total_mv,
+                                        circ_mv=circ_mv)
+
+            self._session.add(history_item)
+        self._session.commit()
     def update_all_stock_history(self):
         '''
         自动更新全部历史数据
@@ -188,13 +270,40 @@ class launcher(object):
                 self._session.commit()
 
     def update_stock_tick(self,security):
+        '''
+        目前仅能更新当日的股票历史分笔数据
+        :param security:
+        :return:
+        '''
         stock = self._session.query(Stock).filter(Stock.security == security).one()  # 获取股票基本信息
-        history_date_list = security._session.query(StockHistory.date).filter(Stock.security == security).all()
-
+        df = ts.get_today_ticks(security)
+        df  = df.set_index('time')
+        for time in df.index:
+            temp = datetime.datetime.today().strftime('%Y-%m-%d')+' '+time
+            date = datetime.datetime.strptime(temp, "%Y-%m-%d %H:%M:%S")
+            stock_tick_item = Tick_data(id = security+'#'+temp,
+                                        time = date,
+                                        price = df.loc[time]['price'],
+                                        change = df.loc[time]['change'],
+                                        volume = df.loc[time]['volume'],
+                                        amount = df.loc[time]['amount'],
+                                        type = unicode(df.ix[time]['type']))
+            try:
+                self._session.query(Tick_data).filter(Tick_data.id == security+'#'+temp).one()
+            except:
+                stock.tick_data.append(stock_tick_item)
+        self._session.commit()
+    def test(self):
+        temp = self._session.query(Trade_calendar).all()
+        print len(temp)
+        #for item in list(temp):
+            #print item
+            #self._session.delete(item)
+        #self._session.commit()
 #if __name__ == '__main__':
 #自动更新数据谷
-l = launcher('test.sqlite3')
-l.update_stocks()#更新股票列表
-l.update_all_stock_history()
-l.fix()
-l.update_HS300s()
+l = launcher()
+#l.update_stocks()#更新股票列表
+#l.update_trade_calendar()
+#l.test()
+l.update_stock_history('000001.SZ')
--- DB/models.py
+++ DB/models.py
@@ -5,38 +5,65 @@
 '''
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column,Integer,String,Float,Date,ForeignKey,DateTime
+from sqlalchemy import Column,Integer,String,Float,Date,ForeignKey,DateTime,Boolean,BigInteger
 from sqlalchemy.orm import relationship
 import os
 Base = declarative_base()
 
+class Trade_calendar(Base):
+    __tablename__ = 'trade_calendar'
+
+    id = Column(String,primary_key=True)
+
+    exchange_id = Column(String)
+    cal_date = Column(Date)
+    isopen = Column(Boolean)
+    pretrade_date = Column(Date)
+
 class Stock(Base):
     __tablename__ = 'stock'
 
-    security = Column(String,primary_key=True)#股票id,主键
+    code = Column(String,primary_key=True)#股票id,主键
+
+    security = Column(String)
     name = Column(String(20))#股票名称
+    exchange_id = Column(String)
+    list_date =  Column(Date)
+    delist_date = Column(Date)
+    list_status = Column(String)
+    is_hs = Column(String)
     history = relationship('StockHistory',backref = 'history')
+    tick_data = relationship('Tick_data',backref = 'tick_data')
 
 class StockHistory(Base):
     __tablename__ = 'stock_history'
 
-    id = Column(String,primary_key=True)#主键,结构是security+date
+    id = Column(String,primary_key=True)#主键,结构是ts_code+date
+
     stock = Column(String,ForeignKey('stock.security'))
+
     date = Column(DateTime)  # 日期
     open = Column(Float)  # 开盘
     close = Column(Float)  # 收盘
     high = Column(Float)  # 最高检
     low = Column(Float)  # 最低价
-    volume = Column(Integer)  # 成交量
     price_change = Column(Float)  # 价格变动
-    p_change = Column(Float)  # 涨跌幅
-    ma5 = Column(Float)  # 5日均价
-    ma10 = Column(Float)
-    ma20 = Column(Float)
-    v_ma5 = Column(Float)  # 5日均量
-    v_ma10 = Column(Float)
-    v_ma20 = Column(Float)
-    turnover = Column(Float)  # 换手率
+    pct_change = Column(Float)  # 涨跌幅
+    volume = Column(Integer)  # 成交量
+    amount = Column(Float)
+
+    turnover_rate = Column(Float)
+    volume_ratio = Column(Float)
+    pe = Column(Float)
+    pe_ttm = Column(Float)
+    pb = Column(Float)
+    ps = Column(Float)
+    ps_ttm = Column(Float)
+    total_share = Column(Float)
+    float_share = Column(Float)
+    free_share = Column(Float)
+    total_mv = Column(Float)
+    circ_mv = Column(Float)
 
 class HS300s(Base):
     __tablename__ = 'HS300s'
@@ -64,8 +91,10 @@ if __name__ =='__main__':
     '''
     在数据库中初始化表
     '''
-    DB_name = 'test.sqlite3'
-    db_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], DB_name)
+    import settings
+    c = settings.ConfigModule()
+    print c.settings
+    db_path = c.settings['DB']['db_path']
     engine = create_engine('sqlite:///%s' % db_path, echo=False, encoding='utf-8')
     Base.metadata.create_all(engine)
     for item in Base.metadata.tables:
